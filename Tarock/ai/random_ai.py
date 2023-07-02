70471eb303721fc668f4721f1b45c227884a559b
thermodynathan <sh4aj@virginia.edu>
2023-07-02 14:15:01 -0400

    implement simple random ai

---
+++ Tarock/ai/random_ai.py
@@ -0,0 +1,21 @@
+from game import *
+from human_vs_ai import *
+
+class RandomAI(TarockAI):
+    def get_move(self, game_state: GameState) -> Tuple[Tuple[int, int], Card]:
+        # get a random card from the hand
+        card = random.choice(game_state.player_hands[game_state.get_next_player()])
+
+        # get all unoccupied cells from the board
+        unoccupied_cell_coords = []
+        for row in range(3):
+            for col in range(3):
+                cell = game_state.board.get_cell_value((row, col))
+                if cell.card is None:
+                    unoccupied_cell_coords.append((row, col))
+
+        # get a random unoccupied cell
+        row, col = random.choice(unoccupied_cell_coords)
+
+        # return the move
+        return (row, col), card
